fe6ca34a670ce3b8d739d2e2fbc88ae84718c108
avascannell <ava_scannell26@milton.edu>
2024-04-29 13:09:20 -0400

    Initial commit

---
+++ src/card.java
@@ -0,0 +1,5 @@
+public class card {
+    //soiut heart, club, spade, diamond
+    //value ex wprth 1-11 points in game
+    // type ex ace, 1,2,
+}
---
+++ src/casino.java
@@ -0,0 +1,10 @@
+public class casino {
+    public static void main(String[] args) {
+        casino c =  new casino();
+    }
+
+    public casino() {
+        System.out.println("welcome to Mx. Bardford's casino!");
+
+    }
+}
