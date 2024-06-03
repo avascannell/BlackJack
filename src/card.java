cf6b8d0c7f0b42996b6aab4dd21c21c7864111bc
avascannell <ava_scannell26@milton.edu>
2024-06-03 14:10:58 -0400

    changes

--- src/card.java
+++ src/card.java
@@ -12,37 +12,76 @@ public class card {
     public card(int pType, int pSuit){
         if(pType == 0){
             type = "ace";
-        } else if(pType == 1){
-            type = "two";
+            value = 11;
         }
+        if(pType == 1){
+            type = "two";
+            value = 2;
+        }
+
+        if(pType == 2){
+            type = "three";
+            value = 3;
+        }
+
+        if(pType == 3){
+            type = "four";
+            value =4;
+        }
+         if(pType == 4){
+            type = "five";
+             value = 5;
+        }
+
+        if(pType == 5){
+            type = "six";
+            value = 6;
+        }
+        if(pType == 6){
+            type = "seven";
+            value = 7;
+        }
+        if(pType == 7){
+            type = "eight";
+            value = 8;
+        }
+        if(pType == 8){
+            type = "nine";
+            value = 9;
+        }
+        if(pType == 9){
+            type = "ten";
+            value = 10;
+        }
+        if(pType == 10){
+            type = "jack";
+            value = 10;
+        }
+        if(pType == 11){
+            type = "queen";
+            value = 10;
+        }
+        if(pType == 12){
+            type = "king";
+            value = 10;
+        }
+
+
         if(pSuit == 1){
             suit = "spades";
-        }else if(pType == 2){
-            type = "three";
         }
         if(pSuit == 2){
             suit = "clubs";
-        }else if(pType == 3){
-            type = "four";
         }
         if(pSuit == 3){
             suit = "hearts";
-        }else if(pType == 4){
-            type = "five";
         }
-        if(pSuit == 4){
+        if(pSuit == 0){
             suit = "diomands";
-        }else if(pType == 5){
-            type = "six";
         }
-
-
-      //suit = pSuit;
-     // type = pType;
-      //TODO; write if statements that set the value of value based on type
-
     }
     public void print(){
         System.out.println("the" + type + "of" + suit + " is worth" + value + "points.");
+
     }
 }
--- src/casino.java
+++ src/casino.java
@@ -7,30 +7,25 @@ public class casino {
     public player p;
     public player dealer;
     public int numCards;
+
     public static void main(String[] args) {
-        casino c =  new casino();
+        casino c = new casino();
 
 
     }
 
 
-    public casino(){
+    public casino() {
         System.out.println("welcome to Mx. Bradford's casino!");
         deck = new card[52];
-        int count = 0;
-        // fill our deck with cards
-        for(int t =0; t < 13; t++){
-            for(int s = 0; s< 4;s++){
-                deck[count] = new card(t, s);
-                count++;
-            }
-        }
+        makeDeck();
+        printDeck();
 
 
         p = new player();
         dealer = new player();
         p.isPlayer = true;
-      //  d = new player();
+        //  d = new player();
         dealer.isPlayer = false;
 
 
@@ -44,54 +39,67 @@ public class casino {
         p.print();
         String desicison = "";
         // stop when they stand or when they bust
-while(!desicison.equals("stand") || p.cardsValue <= 21) {
-    System.out.println("Do you want to stand or hit?");
-    //String desicison = scan.nextLine();
-    System.out.println(desicison);
-    boolean isHit;
-    if (desicison.equals("hit")) {
-        isHit = true;
-        System.out.println("You decided to hit");
-    } else {
-        isHit = false;
-        System.out.println("You decided to stand");
+        while (!desicison.equals("stand") && p.cardsValue <= 21) {
+            System.out.println("Do you want to stand or hit?");
+            desicison = scan.nextLine();
+            System.out.println(desicison);
+            boolean isHit;
+            if (desicison.equals("hit")) {
+                isHit = true;
+                System.out.println("You decided to hit");
+            } else {
+                isHit = false;
+                System.out.println("You decided to stand");
+            }
+            if (isHit) {
+                //take a card from the deck add it to hand
+                // use add card to do this
+                // update the value ]
+                p.addCard(deck[numDealtCards]);
+                numDealtCards++;
+                p.print();
+
+            } else {
+                // dealer has to play
+                // delaer hits untill they reach or surpass 17
+                //use add card
+            }
+        }
     }
-    if (isHit) {
-        //take a card from the deck add it to hand
-        // use add card to do this
-        // update the value ]
-        p.addCard(deck[numDealtCards]);
-        numDealtCards++;
-        p.print();
 
-    } else {
-        // dealer has to play
-        // delaer hits untill they reach or surpass 17
-        //use add card
-    }
-}
-
-
-
-
-
-    }
-    public void makeDeck(){
+    public void makeDeck() {
         deck = new card[52];
+        int count = 0;
+        // fill our deck with cards
+        for (int t = 0; t < 13; t++) {
+            for (int s = 0; s < 4; s++) {
+                deck[count] = new card(t, s);
+                count++;
+            }
+        }
     }
-    public void deal(){
-        // first and second card to pl
-        p.hand [0] = deck[0];
-        p.hand [1] = deck[1];
 
-        dealer.hand[0]= deck[2];
+    public void deal() {
+        // first and second card to pl
+        p.hand[0] = deck[0];
+        p.hand[1] = deck[1];
+
+        dealer.hand[0] = deck[2];
         dealer.hand[1] = deck[3];
         numDealtCards = 4;
 
+    }
 
+    public void printDeck() {
+        for (int i = 0; i < deck.length; i = i + 2) {
+            deck[i].print();
 
-
-
+        }
+    }
+    public void shuffleDeck(){
+        for(int i = 0; i < deck.length; i=i + 2){
+            math.random
+        }
     }
 
 }
